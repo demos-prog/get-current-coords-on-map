c8443a32ac676356ad16f2eb943d44ba1ded0592
Дмитрий <demosne@gmail.com>
2021-06-15 13:52:12 +0300

    init

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import { getMap } from './map.js'
+
+// находим кнопку и добавляем к ней обработчик
+document.getElementById('my_position').onclick = () => {
+  navigator.geolocation.getCurrentPosition(success, error, {
+    enableHighAccuracy: true
+  })
+}
+
+function success({ coords }) {
+  const { latitude, longitude } = coords
+  const currentPosition = [latitude, longitude]
+  // вызываем функцию, передавая ей текущую позицию и сообщение
+  getMap(currentPosition, 'You are here')
+}
+
+function error({ message }) {
+  console.log(message)
+}
